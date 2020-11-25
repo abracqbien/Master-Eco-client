f731037a093c4e54d00511c0a0fc6b15299ba096
ABracqbien <abracqbien@laposte.net>
2020-11-25 16:12:56 +0100

    Branchement header

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import { Switch, Route } from "react-router-dom"
 
 // Components
 import Register from "./pages/auth/Register"
+import Header from "./components/nav/Header"
 import Login from "./pages/auth/Login"
 import Home from "./pages/Home"
 
 const App = () => {
   return (
-    <Switch>
-      <Route exact path="/register" component={Register} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/" component={Home} />
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </>
   )
 }
 
