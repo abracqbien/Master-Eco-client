3a6cd4fa4d6dcc621c796934640274ee5a3cf83e
ABracqbien <abracqbien@laposte.net>
2020-11-25 15:15:29 +0100

    Clean & Home - Register - Login component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react"
+import { Switch, Route } from "react-router-dom"
 
-const App = () => (
-  <div className="App">
-    React app
-  </div>
-)
+// Components
+import Register from "./pages/auth/Register"
+import Login from "./pages/auth/Login"
+import Home from "./pages/Home"
+
+const App = () => {
+
+  return (
+    <div className="App">
+      React app
+    </div>
+  )
+}
 
 export default App
