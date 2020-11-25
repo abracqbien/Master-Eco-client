941dfd5267470f6aca778103b2ed64e908ff244e
ABracqbien <abracqbien@laposte.net>
2020-11-25 15:56:18 +0100

    Add router

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ import Login from "./pages/auth/Login"
 import Home from "./pages/Home"
 
 const App = () => {
-
   return (
-    <div className="App">
-      React app
-    </div>
+    <Switch>
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/" component={Home} />
+    </Switch>
   )
 }
 
